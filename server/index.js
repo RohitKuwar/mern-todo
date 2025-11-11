43836a111abfc036ef13eedfdfbf621ac09e58f6
RohitKuwar <rohit1kuwar@gmail.com>
2025-11-11 22:18:32 +0530

    fixed error

--- server/index.js
+++ server/index.js
@@ -6,52 +6,52 @@ const connectDB = require('./config/db');
 
 const app = express();
 
-// const allowedOrigins = (process.env.ALLOWED_ORIGINS || 'http://localhost:3000')
-//   .split(',')
-//   .map(s => s.trim())
-//   .filter(Boolean);
+const allowedOrigins = (process.env.ALLOWED_ORIGINS || 'http://localhost:3000')
+  .split(',')
+  .map(s => s.trim())
+  .filter(Boolean);
 
-// const corsOptions = {
-//   origin: function(origin, callback) {
-//     // allow requests with no origin (curl, server-to-server)
-//     if (!origin) return callback(null, true);
-//     if (allowedOrigins.indexOf(origin) !== -1) {
-//       return callback(null, true);
-//     }
-//     return callback(new Error('Not allowed by CORS'));
-//   },
-//   methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   credentials: true
-// };
+const corsOptions = {
+  origin: function(origin, callback) {
+    // allow requests with no origin (curl, server-to-server)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      return callback(null, true);
+    }
+    return callback(new Error('Not allowed by CORS'));
+  },
+  methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+};
 
 // Use CORS for all routes (applies response headers)
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: 'http://localhost:3000',
+//   credentials: true,
+// }));
 
 
 // Explicitly handle preflight (OPTIONS) requests
-// app.use((req, res, next) => {
-//   if (req.method === 'OPTIONS') {
-//     // If origin is allowed, echo it back; otherwise block
-//     const origin = req.header('Origin');
-//     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-//       res.header('Access-Control-Allow-Origin', origin || '*');
-//       res.header('Access-Control-Allow-Methods', corsOptions.methods.join(','));
-//       res.header('Access-Control-Allow-Headers', corsOptions.allowedHeaders.join(','));
-//       // if you use credentials, also return:
-//       // res.header('Access-Control-Allow-Credentials', 'true');
-//       return res.sendStatus(200);
-//     } else {
-//       return res.status(403).json({ message: 'CORS blocked' });
-//     }
-//   }
-//   next();
-// });
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    // If origin is allowed, echo it back; otherwise block
+    const origin = req.header('Origin');
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      res.header('Access-Control-Allow-Origin', origin || '*');
+      res.header('Access-Control-Allow-Methods', corsOptions.methods.join(','));
+      res.header('Access-Control-Allow-Headers', corsOptions.allowedHeaders.join(','));
+      // if you use credentials, also return:
+      // res.header('Access-Control-Allow-Credentials', 'true');
+      return res.sendStatus(200);
+    } else {
+      return res.status(403).json({ message: 'CORS blocked' });
+    }
+  }
+  next();
+});
 
 app.use(express.json());
 
