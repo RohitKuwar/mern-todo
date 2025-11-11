9b95f3f58ff97ce1c2f75d12b6f5b285f66e74bc
Rohit Kuwar <70262783+RohitKuwar@users.noreply.github.com>
2025-11-11 16:19:47 +0530

    Add authentication middleware to todos routes

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { getAllTodos, createTodo, updateTodo, patchTodo, deleteTodo, clearAllTodos } = require('../controllers/todosController');
+const auth = require('../middleware/auth');
 
-router.get('/', getAllTodos);
-router.post('/', createTodo);
-router.put('/:id', updateTodo);
-router.patch('/:id', patchTodo);
-router.delete('/:id', deleteTodo);
-router.delete('/', clearAllTodos);
+router.get('/', auth, getAllTodos);
+router.post('/', auth, createTodo);
+router.put('/:id', auth, updateTodo);
+router.patch('/:id', auth, patchTodo);
+router.delete('/:id', auth, deleteTodo);
+router.delete('/', auth, clearAllTodos);
 
 module.exports = router;
