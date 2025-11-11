a9836edda6e04945af0e5701f4e5d7a614c6a238
Rohit Kuwar <70262783+RohitKuwar@users.noreply.github.com>
2025-11-11 16:20:19 +0530

    Create User model with schema and timestamps

---
+++ server/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },         // hashed password
+    refreshTokens: [
+        {
+            token: {
+                type: String
+            },
+            createdAt: {
+                type: Date,
+                default: Date.now,
+            }  // store active refrsh tokens
+        }
+    ]
+}, { timestamps: true })
+
+module.exports = mongoose.model('User', UserSchema);
